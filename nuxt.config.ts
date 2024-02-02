42046d600bdaf05469d1e78efa7cc276b897b4a7
YassineZhang <57878778@qq.com>
2024-02-02 17:29:43 +0800

    feat: The built resource is published to the gh-pages branch when the deployment command is executed.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,4 +13,10 @@ export default defineNuxtConfig({
      */
     componentDir: "./components/ui",
   },
+  /** 设定基础URL，当处于生成环境也就是在github-pages中能够正常访问 */
+  app: {
+    baseURL:
+      process.env.NODE_ENV === "production" ? "/background-template/" : "/",
+    buildAssetsDir: "/static/",
+  },
 });

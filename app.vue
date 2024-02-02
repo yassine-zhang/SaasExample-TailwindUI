5a77a9162bd86a00402560ab8ea5f1c5f149645f
YassineZhang <57878778@qq.com>
2024-02-02 19:25:17 +0800

    fix: Fixed ico icon not displayed in github-pages

--- app.vue
+++ app.vue
@@ -2,4 +2,10 @@
   <AppHeader />
   <NuxtPage></NuxtPage>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+  link() {
+    return [{ rel: "icon", type: "image/png", href: "/favicon.ico" }];
+  },
+});
+</script>

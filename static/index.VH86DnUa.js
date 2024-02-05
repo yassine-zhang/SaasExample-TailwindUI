4cb73ff0202d08b88b11881eb6723cbaee2eaad6
YassineZhang <57878778@qq.com>
2024-02-06 00:43:46 +0800

    Updates

--- static/index.VH86DnUa.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper.x3n3nnut.js";import{l as r,k as o}from"./entry.QFVJ2vW6.js";const s={},a={class:"border-4 border-dashed border-gray-200 rounded-lg h-96"};function c(d,t){return o(),r("div",a," Dashboard Page ")}const f=e(s,[["render",c]]);export{f as default};
